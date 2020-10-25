7d5eba3c2d71cf5f345443cdea1983766099f860
juansg3701 <54075068+juansg3701@users.noreply.github.com>
2020-10-25 00:46:15 -0500

    .

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
 
       <link  rel="icon"   href="img/Logo1.jpeg" type="image/jpeg" />
 
-    <title>POS</title>
+    <title>1A</title>
 
     <!-- Fonts -->
      <!-- Font Awesome -->
@@ -43,21 +43,18 @@
 
                 <!-- Branding Image -->
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    POS
+                    1A
                 </a>
             </div>
 
             <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                <!-- Left Side Of Navbar -->
-                <ul class="nav navbar-nav">
-                    <li><a href="{{ url('/home') }}">Inicio sesión</a></li>
-                </ul>
+               
 
                 <!-- Right Side Of Navbar -->
                 <ul class="nav navbar-nav navbar-right">
                     <!-- Authentication Links -->
                     @if (Auth::guest())
-                        <li><a href="{{ url('/login') }}">Ingresar</a></li>
+                        <li><a href="{{ url('/') }}">Inicio de sesión</a></li>
                     @else
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
