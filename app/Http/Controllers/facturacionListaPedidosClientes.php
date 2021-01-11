d4fc1f4d3f62d3aeb3e6cc7118f1f994e9cbb203
juansg3701 <54075068+juansg3701@users.noreply.github.com>
2021-01-10 21:13:03 -0500

    .

--- app/Http/Controllers/facturacionListaPedidosClientes.php
+++ app/Http/Controllers/facturacionListaPedidosClientes.php
@@ -285,7 +285,7 @@ class facturacionListaPedidosClientes extends Controller
 	 		$pedidoCliente->porcentaje_venta=$request->get('porcentaje_venta');
 	 		$pedidoCliente->cliente_id_cliente=$request->get('cliente_id_cliente');
 	 		$pedidoCliente->empleado_id_empleado=$request->get('empleado_id_empleado');
-			$pedidoCliente->tipo_pago_id_tpago=$request->get('tipo_pago_id_tpago');
+			$pedidoCliente->tipo_pago_id_tpago=1;
 			$pedidoCliente->sede_id_sede=$request->get('sede_id_sede');
 			$pedidoCliente->finalizar=$request->get('finalizar');
 	 		$pedidoCliente->save();
