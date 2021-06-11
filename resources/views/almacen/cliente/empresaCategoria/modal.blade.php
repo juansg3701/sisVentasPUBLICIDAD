289a847c62206ccbc35f8419028cf959b3e0faa2
juansg3701 <54075068+juansg3701@users.noreply.github.com>
2021-06-10 21:19:33 -0500

    cambios de subempresas a aliados

--- resources/views/almacen/cliente/empresaCategoria/modal.blade.php
+++ resources/views/almacen/cliente/empresaCategoria/modal.blade.php
@@ -7,14 +7,14 @@ role="dialog" tabindex="-1" id="modal-delete-{{$em->id_empresa_categoria}}">
 		<div class="modal-content">
 			<div class="modal-header">
 				
-                <h4 class="modal-title">Eliminar subempresa</h4>
+                <h4 class="modal-title">Eliminar aliado</h4>
 				<button type="button" class="close" data-dismiss="modal" 
 				aria-label="Close">
                      <span aria-hidden="true">×</span>
                 </button>
 			</div>
 			<div class="modal-body">
-				<p>¿Desea eliminar la subempresa?</p>
+				<p>¿Desea eliminar el aliado?</p>
 			</div>
 			<div class="modal-footer">
 				<button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
